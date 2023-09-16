a067db25416816dedbc46c50323aeaf9558ff74f
zimrancis <zimrancis@gmail.com>
2023-09-17 05:45:10 +0600

    Delete video_010-secure-microservices-with-resourceserver-and-oauth2 directory

--- video_010-secure-microservices-with-resourceserver-and-oauth2/profile-service/src/main/java/com/krishantha/rentcloud/profileservice/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.krishantha.rentcloud.profileservice.repository;
-
-
-import com.krishantha.rentcloud.commons.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
-}
